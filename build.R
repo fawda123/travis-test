d107ececf619a2df1b70e11374fcbb66dcb0bb2c
fawda123 <mbafs2012@gmail.com>
2020-05-04 09:17:18 -0400

    trying different file download

--- build.R
+++ build.R
@@ -1,5 +1,6 @@
 tmp <- tempfile()
-url <- "ftp://ftp.FreeBSD.org/pub/FreeBSD/README.TXT"
+# url <- "ftp://ftp.FreeBSD.org/pub/FreeBSD/README.TXT"
+url <- "ftp://ftp.floridamarine.org/users/fim/tmac/NektonIndex/TampaBay_NektonIndexData.csv"
 download.file(url, tmp, quiet = TRUE)
 lines <- readLines(tmp)
 file.remove(tmp)
