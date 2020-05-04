a9beabdedbeba61e5f193826d92055778d89fb50
fawda123 <mbafs2012@gmail.com>
2020-05-04 09:25:08 -0400

    back to orig url

--- build.R
+++ build.R
@@ -1,6 +1,5 @@
 tmp <- tempfile()
-# url <- "ftp://ftp.FreeBSD.org/pub/FreeBSD/README.TXT"
-url <- "ftp://ftp.floridamarine.org/users/fim/tmac/NektonIndex/TampaBay_NektonIndexData.csv"
+url <- "ftp://ftp.FreeBSD.org/pub/FreeBSD/README.TXT"
 download.file(url, tmp, quiet = TRUE)
 lines <- readLines(tmp)
 file.remove(tmp)
